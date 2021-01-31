50ff683802753726e32477e705b6c725237eaa1b
imperator30000 <vadim095311@mail.ru>
2021-01-31 10:50:54 +0300

    123

---
+++ test_hits.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__()
+        self.im = pygame.load(pygame.image.load('персонажи/Reptile/идет/вправо/0.png'))
+
+        self.rect = self.im.get_rect()
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__()
+        self.im = pygame.load(pygame.image.load('sprite/pygame_idle.png'))
+
+        self.rect = self.im.get_rect()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600,  600), pygame.RESIZABLE)
+
+    fps = 90
+
+    while
